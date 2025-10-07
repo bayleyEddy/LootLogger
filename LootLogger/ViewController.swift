3c1f0e5491cb1a9ad1e21fc875093c7670d22269
bayley <bayley019@outlook.com>
2025-10-07 15:34:26 -0400

    Initial Commit

---
+++ LootLogger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LootLogger
+//
+//  Created by bayley on 10/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LootLoggerTests/LootLoggerTests.swift
@@ -0,0 +1,17 @@
+//
+//  LootLoggerTests.swift
+//  LootLoggerTests
+//
+//  Created by bayley on 10/7/25.
+//
+
+import Testing
+@testable import LootLogger
+
+struct LootLoggerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
