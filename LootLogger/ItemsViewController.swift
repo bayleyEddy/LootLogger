ea0a088d11be980d48b8e492d6d010bc5fcf969d
bayley <bayley019@outlook.com>
2025-11-04 22:27:33 -0500

    Complete prior to Bronze challenge ch.10

--- LootLogger/ItemsViewController.swift
+++ LootLogger/ItemsViewController.swift
@@ -93,6 +93,8 @@ class ItemsViewController: UITableViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.rowHeight = 65
+        //tableView.rowHeight = 65
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 65
     }
 }
